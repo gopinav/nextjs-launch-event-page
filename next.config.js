2ed7d76d99498fdb1b9f97dec0b7d157ac0b787c
Vishwas <vishwas.g5@gmail.com>
2023-09-22 15:50:10 +0530

    Update config for assets

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    domains: ["cdn.builder.io"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
