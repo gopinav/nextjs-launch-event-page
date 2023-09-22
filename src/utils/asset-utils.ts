11b97776512054feca0844c8aeea6b6139d01606
Vishwas <vishwas.g5@gmail.com>
2023-09-22 17:52:43 +0530

    Add figma

--- src/utils/asset-utils.ts
+++ src/utils/asset-utils.ts
@@ -3,6 +3,8 @@ export const assets = {
     "https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2F266e05dba3864799b4715cf4bfd8aa2a",
   square:
     "https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2F8997f779f33b430bb22ca667d1b73ade",
+  figma:
+    "https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2Fe82b4bbd5a7c45b49897b9570893dfe2",
   react:
     "https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2F95c70a06f779481a957c2b384e3a270d",
   svelte:
